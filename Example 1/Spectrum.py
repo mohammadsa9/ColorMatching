Fix XYZ calculation

Example 1/Spectrum.py
@@ -74,31 +74,31 @@ class Observation:
             object.light = object
 
     def getK(object):
-        k = 100/np.sum(object.E.dot(object.ybar.transpose()))
+        k = 100/(np.sum((object.E)*(object.ybar)))
         return k
 
     def getX(object):
         if np.isscalar(object.R):
-            x = object.k * np.sum(object.E.dot(object.xbar.transpose()))
+            R = 1
         else:
-            x = object.k * \
-                np.sum(object.E.dot(object.xbar.transpose()).dot(object.R))
+            R = object.R
+        x = object.k * np.sum((object.E)*(object.xbar) * (R))
         return x
 
     def getY(object):
         if np.isscalar(object.R):
-            y = object.k * np.sum(object.E.dot(object.ybar.transpose()))
+            R = 1
         else:
-            y = object.k * \
-                np.sum(object.E.dot(object.ybar.transpose()).dot(object.R))
+            R = object.R
+        y = object.k * np.sum((object.E)*(object.ybar) * (R))
         return y
 
     def getZ(object):
         if np.isscalar(object.R):
-            z = object.k * np.sum(object.E.dot(object.zbar.transpose()))
+            R = 1
         else:
-            z = object.k * \
-                np.sum(object.E.dot(object.zbar.transpose()).dot(object.R))
+            R = object.R
+        z = object.k * np.sum((object.E)*(object.zbar) * (R))
         return z
 
     def getL(object):

Example 2/Functions.py
@@ -71,7 +71,7 @@ class Observation:
         object.LightSource = LightSource
         object.Viewer = Viewer
         object.E = LightSource.E
-        object.R = R
+        object.R = np.array([R]).T
         object.xbar = Viewer.xbar
         object.ybar = Viewer.ybar
         object.zbar = Viewer.zbar
@@ -82,31 +82,31 @@ class Observation:
             object.light = object
 
     def getK(object):
-        k = 100/np.sum(object.E.dot(object.ybar.transpose()))
+        k = 100/(np.sum((object.E)*(object.ybar)))
         return k
 
     def getX(object):
         if np.isscalar(object.R):
-            x = object.k * np.sum(object.E.dot(object.xbar.transpose()))
+            R = 1
         else:
-            x = object.k * \
-                np.sum(object.E.dot(object.xbar.transpose()).dot(object.R))
+            R = object.R
+        x = object.k * np.sum((object.E)*(object.xbar) * (R))
         return x
 
     def getY(object):
         if np.isscalar(object.R):
-            y = object.k * np.sum(object.E.dot(object.ybar.transpose()))
+            R = 1
         else:
-            y = object.k * \
-                np.sum(object.E.dot(object.ybar.transpose()).dot(object.R))
+            R = object.R
+        y = object.k * np.sum((object.E)*(object.ybar) * (R))
         return y
 
     def getZ(object):
         if np.isscalar(object.R):
-            z = object.k * np.sum(object.E.dot(object.zbar.transpose()))
+            R = 1
         else:
-            z = object.k * \
-                np.sum(object.E.dot(object.zbar.transpose()).dot(object.R))
+            R = object.R
+        z = object.k * np.sum((object.E)*(object.zbar) * (R))
         return z
 
     def getL(object):
@@ -201,7 +201,7 @@ def distance(x1, x2):
 def myInterploration(OBS, munsell_XYZ, munsell_R):
     light_source = OBS.LightSource
     viewer = OBS.Viewer
-    R_sample = OBS.R
+    R_sample = OBS.R[0]
     sample = Observation(light_source, viewer, R_sample)
     sample_XYZ = [sample.getX(), sample.getY(), sample.getZ()]
     XYZ_BEST = np.array(
@@ -242,7 +242,7 @@ def Interploration(calc, OBS, munsell_R):
 
 
 def revInterploration(calc, OBS, munsell_XYZ):
-    R_sample = OBS.R
+    R_sample = OBS.R[0]
     R_s = R_sample.T[0]
     checker = calc.possible(R_s)
 

Example 2/main.py
@@ -35,7 +35,10 @@ munsell_R = []
 data = pd.read_excel('Munsell400_10_700.xlsx')
 for i in range(munsell_size):
     newdata = data[data.columns[i]]
-    munsell_R.append(newdata.to_numpy())
+    newdata = newdata.to_numpy()
+    #newdata = np.array([newdata])
+    munsell_R.append(newdata)
+
 
 data = pd.read_excel('sample-data.xlsx')
 extract_data = pd.DataFrame(data, columns=['xbar'])
@@ -67,6 +70,8 @@ Getting Data is Finished
 
 # Calculate XYZ for Munsell R values
 munsell_XYZ = []
+temp = Observation(light_source, viewer, 1)
+
 for i in range(munsell_size):
     temp = Observation(light_source, viewer, munsell_R[i])
     temp_arr = [temp.getX(), temp.getY(), temp.getZ()]
@@ -90,7 +95,7 @@ calc = MyDelaunay(points)
 for i in range(14):
     OBS = Observation(light_source, viewer, samples[i])
     R_calc, check = Interploration(calc, OBS, munsell_R)
-    #R_calc2 = myInterploration(OBS, munsell_XYZ, munsell_R)
+    # R_calc2 = myInterploration(OBS, munsell_XYZ, munsell_R)
     CAL = Observation(light_source, viewer, R_calc)
     compare = Compare(OBS, CAL)
     RMS = compare.RMS()
@@ -100,11 +105,11 @@ for i in range(14):
         gamut = 'out-of-gamut data'
     text = 'Sample'+str(i+1)+': '+gamut+' , ΔE = ' + \
         str(DeltaE)+', RMS = '+str(round(RMS, 6))
-    R_sample = OBS.R
+    R_sample = OBS.R[0]
     # Showing Data
     p1, = plt.plot(wave_length, R_sample, color='green', label="R STD")
     p2, = plt.plot(wave_length, R_calc, color='red', label="R Interpolation")
-    #p3, = plt.plot(wave_length, R_calc2[0], color='black', label="R 3")
+    # p3, = plt.plot(wave_length, R_calc2[0], color='black', label="R 3")
     lines = [p1, p2]
     plt.legend(lines, [l.get_label() for l in lines])
     plt.gcf().canvas.set_window_title('Delaunay Calculation - Sample: '+str(i+1))
@@ -123,7 +128,7 @@ points = np.array(munsell_R)
 calc = MyDelaunay(points)
 for i in range(14):
     OBS = Observation(light_source, viewer, samples[i])
-    #OBS = Observation(light_source, viewer, test)
+    # OBS = Observation(light_source, viewer, test)
     XYZ_calc, check = revInterploration(calc, OBS, munsell_XYZ)
     gamut = 'in-gamut data'
     if check is False: